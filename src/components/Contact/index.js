997eb36dcd72f9c06bc1328028c478b8e43ffa7e
Erikson <chris.s.erickson@gmail.com>
2022-10-29 09:40:04 -0400

    form handlers

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,52 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { useState } from "react";
 
 function ContacForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <htmlFor htmlFor="name">Name:</htmlFor>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <htmlFor htmlFor="email">Email address:</htmlFor>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            defaultValue={email}
+            onChange={handleChange}
+            name="email"
+          />
         </div>
         <div>
           <htmlFor htmlFor="message">Message:</htmlFor>
-          <textarea name="message" rows="5" />
+          <textarea
+            name="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
         </div>{" "}
         <button type="submit">Submit</button>
       </form>
